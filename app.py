d936ed34ae11b6d895ca2cd508b86f4386dc47d1
Deveshree Ghanshyam Bhakkad <bhakkaddeveshree@gmail.com>
2026-01-12 22:33:28 +0530

    Real-Time Emotion Analytics Dashboard

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import time
 
 # -------------------- PAGE CONFIG --------------------
 
-st.set_page_config(
+t.set_page_config(
     page_title="Emotion Analytics Dashboard",
     layout="wide"
 )
